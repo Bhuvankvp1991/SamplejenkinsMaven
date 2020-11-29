667c42595b6683ddc9aeff384dba62377349dd40
Bhuvanesh <kabhuvanesh91@gmail.com>
2020-11-29 21:30:21 +0530

    Demo ist commit

---
+++ SE/src/main/java/DemoPack/LeafTaps.java
@@ -0,0 +1,18 @@
+package DemoPack;
+
+public class LeafTaps {
+
+	@Test
+	public void startLeaf() {	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.close();
+	}
+}
